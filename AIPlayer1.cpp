7367ef5ae7130754a9e3ab736741ea8ee26322d5
Narcissus2 <DMT1709533@xmu.edu.my>
2018-11-13 09:51:16 +0800

    Update AIPlayer1.cpp

---
+++ AIPlayer1.cpp
@@ -0,0 +1,31 @@
+#include "AIPlayer1.h"
+using namespace std;
+/*
+void AIPlayer1::initAIPar(ChessType color)
+    {
+        chessColor = color;
+        for(int i = 0; i < 15; i++)
+        {
+            for(int j = 0; j < 15; j++)
+            {
+                Score[i][j] = 0;
+            }
+        }
+        toScore["_aa_"]=100;
+        toScore["aa_"]=50;
+        toScore["_aa"]=50;
+
+        toScore["_aaa_"]=1000;
+        toScore["aaa_"]=500;
+        toScore["_aaa"]=500;
+
+        toScore["_aaaa_"]=10000;
+        toScore["aaaa_"]=5000;
+        toScore["_aaaa"]=5000;
+
+        toScore["aaaaa"]=FLT_MAX;
+        toScore["aaaaa_"]=FLT_MAX;
+        toScore["_aaaaa"]=FLT_MAX;
+        toScore["_aaaaa_"]=FLT_MAX;
+    }
+*/
