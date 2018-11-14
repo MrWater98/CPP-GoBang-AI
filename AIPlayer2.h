8369d54090eed4858073a44a5f7ec78b4e7ebf57
Narcissus2 <DMT1709533@xmu.edu.my>
2018-11-14 23:37:37 +0800

    Update AIPlayer2.h

---
+++ AIPlayer2.h
@@ -0,0 +1,16 @@
+#ifndef AIPLAYER2_H
+#define AIPLAYER2_H
+
+
+class AIPlayer2
+{
+    public:
+        AIPlayer2();
+        virtual ~AIPlayer2();
+
+    protected:
+
+    private:
+};
+
+#endif // AIPLAYER2_H
