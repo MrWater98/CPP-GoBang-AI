50ea5c9f8de44f6191209ce2cccee57a7276da97
Narcissus2 <DMT1709533@xmu.edu.my>
2018-11-15 01:40:00 +0800

    Update AIPlayer2.h

--- AIPlayer2.h
+++ AIPlayer2.h
@@ -1,11 +1,13 @@
 #ifndef AIPLAYER2_H
 #define AIPLAYER2_H
+#include "AIPlayer1.h"
 
-
-class AIPlayer2
+class AIPlayer2 : public AIPlayer1
 {
     public:
         AIPlayer2();
+        AIPlayer2(ChessType Color);
+        float getLineScore(pair<short,short> p,pair<short,short> offset,ChessType myChessColor);
         virtual ~AIPlayer2();
 
     protected:
