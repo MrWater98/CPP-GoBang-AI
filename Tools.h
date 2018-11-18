fe128a0a34b7bb36725958fbaf4b4c188473b3c9
Narcissus2 <DMT1709533@xmu.edu.my>
2018-11-18 21:19:03 +0800

    Update Tools.h

--- Tools.h
+++ Tools.h
@@ -7,4 +7,5 @@ void HideCursor();
 void SetCursorPos(pair<short,short> pos);
 float getTotalScore(pair<short,short> p);
 float getLineScore(pair<short,short> p,pair<short,short> offset);
+void SetColor(int colorID);//设置文本颜色
 #endif // TOOLS_H
