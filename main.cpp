3905bbdf6dcb795e48535b41679cf7120571e41b
Narcissus2 <DMT1709533@xmu.edu.my>
2018-11-22 09:56:02 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,22 +6,23 @@
 #include "Tools.h"
 #include "AIPlayer1.h"
 #include "AIPlayer2.h"
+#include "AIPlayer3.h"
 using namespace std;
 int main()
 {
     HideCursor();
-    Player BlackPlayer(BLACK);
-    AIPlayer2 WhitePlayer(WHITE);
+    AIPlayer2 BlackPlayer(BLACK);
+    AIPlayer3 WhitePlayer(WHITE);
 
     while(true)
     {
-        //ChessBoard::getInstance()->print(pair<short,short>(0,0));
+        ChessBoard::getInstance()->print(pair<short,short>(0,0));
         BlackPlayer.Start();
-        //Sleep(200);
-        //system("cls");
-        //ChessBoard::getInstance()->print(pair<short,short>(0,0));
+        Sleep(200);
+        system("cls");
+        ChessBoard::getInstance()->print(pair<short,short>(0,0));
         WhitePlayer.Start();
-        //Sleep(200);
+        Sleep(200);
     }
     return 0;
 }
