3a537ead7294387bf6d2477de55aef9e91644840
Narcissus2 <DMT1709533@xmu.edu.my>
2018-12-13 19:26:40 +0800

    Create AIPlayer5.h

---
+++ AIPlayer5.h
@@ -0,0 +1,40 @@
+#ifndef AIPLAYER5_H
+#define AIPLAYER5_H
+#include<iostream>
+#include<map>
+#include<algorithm>
+#include "Player.h"
+#include "Tools.h"
+using namespace std;
+struct MiniMaxNode2
+{
+    public:
+    ChessType chess;
+    pair<short,short> pos;
+    vector<MiniMaxNode2> child;
+    float value;//得到下了这步后局面的分数
+    float point;//得到这步棋的分数
+};
+class AIPlayer5 : public Player
+{
+    public:
+        AIPlayer5(ChessType Color);
+        AIPlayer5();
+        map<string,int> evaBoard;
+        map<ChessType,char> stateMap;
+        map<string,int> chessModel;
+        bool hasNeighbor(char chessBoard[][15],int i,int j);
+        float getTotalValue(char chessBoard[][15],bool myself);
+        float getLineScore(char chessboard[][15],pair<short,short> p,pair<short,short> offset,ChessType myChessColor);
+        float AlphaBeta(MiniMaxNode2 node,int depth,bool myself,float alpha,float beta);
+        vector<MiniMaxNode2> GetVector(char chessboard[][15],ChessType myChessColor,bool myself);
+        void createTree(MiniMaxNode2 &node,char chessboard[][15],int depth,bool myself);
+        void playChess();
+        virtual ~AIPlayer5();
+
+    protected:
+
+    private:
+};
+
+#endif // AIPLAYER5_H
