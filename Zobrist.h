ab856cecec0a4eab18c1d4eab4d8198167425ed3
Narcissus2 <DMT1709533@xmu.edu.my>
2018-12-23 22:48:39 +0800

    Create Zobrist.h

---
+++ Zobrist.h
@@ -0,0 +1,16 @@
+#ifndef ZOBRIST_H
+#define ZOBRIST_H
+
+
+class Zobrist
+{
+    public:
+        Zobrist();
+        virtual ~Zobrist();
+
+    protected:
+
+    private:
+};
+
+#endif // ZOBRIST_H
