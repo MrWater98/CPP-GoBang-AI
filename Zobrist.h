cb22b5c8c800296328cd14fc05b7d1e5257cf07b
Narcissus2 <DMT1709533@xmu.edu.my>
2018-12-23 23:57:46 +0800

    Update Zobrist.h

--- Zobrist.h
+++ Zobrist.h
@@ -1,13 +1,21 @@
 #ifndef ZOBRIST_H
 #define ZOBRIST_H
-
-
+#include<unordered_map>
+using namespace std;
 class Zobrist
 {
     public:
         Zobrist();
         virtual ~Zobrist();
+        unsigned long long int ZobristTable[15][15][2];
+        // Generates a Randome number from 0 to 2^64-1
+        unsigned long long int randomInt();
+        // This function associates each piece with a number
+        int indexOf(char piece);
+        // Computes the hash value of a given board
+        unsigned long long int computeHash(char board[15][15]);
 
+        unordered_map<string,float> hashTable;
     protected:
 
     private:
