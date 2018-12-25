3064b9d9748186e3c3a2eccd16979747bba2719a
Narcissus2 <DMT1709533@xmu.edu.my>
2018-12-25 15:25:34 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include"Player.h"
-#include"AIPlayer1.h"
 #include<windows.h>
 #include "GameController.h"
 #include "Tools.h"
@@ -9,23 +8,32 @@
 #include "AIPlayer3.h"
 #include "AIPlayer4.h"
 #include "AIPlayer5.h"
-#pragma GCC optimize(2)
-using namespace std;
+#pragma GCC optimize(3)
+#include "GameController.h"
+
+int main()//程序入口
+{
+    GameController c;//声明一个Controller类
+    c.Game();//整个游戏循环
+    return 0;
+}
+/*using namespace std;
 int main()
 {
     HideCursor();
-    Player BlackPlayer(BLACK);
-    AIPlayer5 WhitePlayer(WHITE);
+    AIPlayer3 BlackPlayer(BLACK);
+    AIPlayer4 WhitePlayer(WHITE);
     while(true)
     {
-        //ChessBoard::getInstance()->print(pair<short,short>(0,0));
+        ChessBoard::getInstance()->print(pair<short,short>(0,0));
         BlackPlayer.Start();
-        //Sleep(200);
-        //system("cls");
-        //ChessBoard::getInstance()->print(pair<short,short>(0,0));
+        Sleep(200);
+        system("cls");
+        ChessBoard::getInstance()->print(pair<short,short>(0,0));
         WhitePlayer.Start();
-        //Sleep(200);
+        Sleep(200);
     }
     getchar();
     return 0;
 }
+*/
