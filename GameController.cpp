64d005fb774b330d4ba24d10369067b0a4168f21
Narcissus2 <DMT1709533@xmu.edu.my>
2018-12-25 15:25:27 +0800

    Update GameController.cpp

--- GameController.cpp
+++ GameController.cpp
@@ -1,21 +1,361 @@
 #include "GameController.h"
+#include <iostream>
+#include <time.h>
+#include <conio.h>
+#include "GameController.h"
+#include "Player.h"
+#include "GameController.h"
+#include "Tools.h"
+#include "startInterface.h"
+#include <windows.h>
+#include "AIPlayer1.h"
+#include "AIPlayer2.h"
+#include "AIPlayer3.h"
+#include "AIPlayer4.h"
+#include "AIPlayer5.h"
+#pragma GCC optimize(3)
 
-GameController::GameController()
+void GameController::Start()//开始界面
 {
-    //ctor
+    SetWindowSize(41, 32);//设置窗口大小
+    SetColor(2);//设置开始动画颜色
+    StartInterface *start = new StartInterface();//动态分配一个StartInterface类start
+    start->Action();//开始动画
+    delete start;//释放内存空间
+
+    /*设置关标位置，并输出提示语，等待任意键输入结束*/
+    SetCursorPosition(13, 26);
+    std::cout << "Press any key to start... " ;
+    SetCursorPosition(13, 27);
+    system("pause");
 }
-
-GameController::~GameController()
+void GameController::SelectPlayer()//选择和人打还是和AI打
 {
-    //dtor
-}
-GameController* GameController::instance = nullptr;
-GameController* GameController::getInstance()
-{
+    /*初始化界面选项*/
+    SetColor(3);
+    SetCursorPosition(13, 26);
+    std::cout << "                          " ;
+    SetCursorPosition(13, 27);
+    std::cout << "                          " ;
+    SetCursorPosition(6, 21);
+    std::cout << "Chose to Play Game!" ;
+    SetCursorPosition(6, 22);
+    std::cout << "(UP/DOWN move, Enter determine.)" ;
+    SetCursorPosition(27, 22);
+    SetBackColor();//第一个选项设置背景色以表示当前选中
+    std::cout << "Player VS Player" ;
 
-    if(!instance)
+    SetCursorPosition(27, 26);
+    SetColor(3);
+    std::cout << "Player VS     AI" ;
+
+    SetCursorPosition(0, 31);
+
+    /*上下方向键选择模块*/
+    int ch;//记录键入值
+    key = 1;//记录选中项，初始选择第一个
+    bool flag = false;//记录是否键入Enter键标记，初始置为否
+    while ((ch = getch()))
     {
-        instance = new GameController();
+        switch (ch)//检测输入键
+        {
+        case 72://UP上方向键
+            if (key = 2)//当此时选中项为第一项时，UP上方向键无效
+            {
+                SetCursorPosition(27, 22);//给待选中项设置背景色
+                SetBackColor();
+                std::cout << "Player VS Player" ;
+
+                SetCursorPosition(27, 26);//将已选中项取消我背景色
+                SetColor(3);
+                std::cout << "Player VS     AI" ;
+                --key;
+            }
+            break;
+
+        case 80://DOWN下方向键
+            if (key = 1)
+            {
+                SetCursorPosition(27, 26);//给待选中项设置背景色
+                SetBackColor();
+                std::cout << "Player VS     AI" ;
+
+                SetCursorPosition(27, 22);//将已选中项取消我背景色
+                SetColor(3);
+                std::cout << "Player VS Player" ;
+                key++;
+            }
+            break;
+
+        case 13://Enter回车键
+            flag = true;
+            break;
+        default://无效按键
+            break;
+        }
+        if (flag) break;//输入Enter回车键确认，退出检查输入循环
+
+        SetCursorPosition(0, 31);//将光标置于左下角，避免关标闪烁影响游戏体验
     }
-    return instance;
+    switch (key)
+    {
+    case 1:
+    {
+        //直接开始和人对打
+        system("cls");
+        HideCursor();//隐藏鼠标
+        Player BlackPlayer(BLACK);
+        Player WhitePlayer(WHITE);
+        while(true)//游戏循环
+        {
+            BlackPlayer.Start();
+            WhitePlayer.Start();
+        }
+        break;
+    }
+    case 2:
+    {
+        SelectFirstPlayer();
+        break;
+    }
+    }
 }
+void GameController::SelectFirstPlayer()
+{
+    SetCursorPosition(27, 22);
+    cout<<"                                          ";
+    SetCursorPosition(27, 22);
+    SetBackColor();//第一个选项设置背景色以表示当前选中
+    std::cout << "You First" ;
+    SetCursorPosition(27, 26);
+    SetColor(3);
+    cout<<"                                          ";
+    SetColor(3);
+    SetCursorPosition(27, 26);
+    std::cout << "AI  First" ;
+
+    SetCursorPosition(0, 31);
+
+    /*上下方向键选择模块*/
+    int ch;//记录键入值
+    key = 1;//记录选中项，初始选择第一个
+    bool flag = false;//记录是否键入Enter键标记，初始置为否
+    while ((ch = getch()))
+    {
+        switch (ch)//检测输入键
+        {
+        case 72://UP上方向键
+            if (key = 2)//当此时选中项为第一项时，UP上方向键无效
+            {
+                SetCursorPosition(27, 22);//给待选中项设置背景色
+                SetBackColor();
+                std::cout << "You First" ;
+
+                SetCursorPosition(27, 26);//将已选中项取消我背景色
+                SetColor(3);
+                std::cout << "AI  First" ;
+                --key;
+            }
+            break;
+
+        case 80://DOWN下方向键
+            if (key = 1)
+            {
+                SetCursorPosition(27, 26);//给待选中项设置背景色
+                SetBackColor();
+                std::cout << "AI  First" ;
+
+                SetCursorPosition(27, 22);//将已选中项取消我背景色
+                SetColor(3);
+                std::cout << "You First" ;
+                key++;
+            }
+            break;
+
+        case 13://Enter回车键
+            flag = true;
+            break;
+        default://无效按键
+            break;
+        }
+        if (flag) break;//输入Enter回车键确认，退出检查输入循环
+
+        SetCursorPosition(0, 31);//将光标置于左下角，避免关标闪烁影响游戏体验
+    }
+    switch (key)
+    {
+    case 1:
+        //人类先下棋
+        SelectDifficulty();
+        break;
+    case 2:
+        SelectDifficulty();
+        //AI先下棋
+        break;
+    }
+}
+void GameController::SelectDifficulty()//选择界面
+{
+    SetColor(3);
+    SetCursorPosition(13, 26);
+    std::cout << "                          " ;
+    SetCursorPosition(13, 27);
+    std::cout << "                          " ;
+    SetCursorPosition(6, 21);
+    std::cout << "Chose to Play Game!" ;
+    SetCursorPosition(6, 22);
+    std::cout << "(UP/DOWN move, Enter determine.)" ;
+    SetCursorPosition(27, 22);
+    cout<<"                                          ";
+    SetCursorPosition(27, 22);
+    SetBackColor();//第一个选项设置背景色以表示当前选中
+    std::cout << "Easy   Mode" ;
+    SetCursorPosition(27, 24);
+    SetColor(3);
+    std::cout << "Normal Mode" ;
+    SetCursorPosition(27, 26);
+    cout<<"                                          ";
+    SetCursorPosition(27, 26);
+    std::cout << "Hard   Mode" ;
+    SetCursorPosition(27, 28);
+    std::cout << "Purgatory!!" ;
+    SetCursorPosition(0, 31);
+
+    /*上下方向键选择模块*/
+    int ch;//记录键入值
+    key = 1;//记录选中项，初始选择第一个
+    bool flag = false;//记录是否键入Enter键标记，初始置为否
+    while ((ch = getch()))
+    {
+        switch (ch)//检测输入键
+        {
+        case 72://UP上方向键
+            if (key > 1)//当此时选中项为第一项时，UP上方向键无效
+            {
+                switch (key)
+                {
+                case 2:
+                    SetCursorPosition(27, 22);//给待选中项设置背景色
+                    SetBackColor();
+                    std::cout << "Easy   Mode" ;
+
+                    SetCursorPosition(27, 24);//将已选中项取消我背景色
+                    SetColor(3);
+                    std::cout << "Normal Mode" ;
+
+                    --key;
+                    break;
+                case 3:
+                    SetCursorPosition(27, 24);
+                    SetBackColor();
+                    std::cout << "Normal Mode" ;
+
+                    SetCursorPosition(27, 26);
+                    SetColor(3);
+                    std::cout << "Hard   Mode" ;
+
+                    --key;
+                    break;
+                case 4:
+                    SetCursorPosition(27, 26);
+                    SetBackColor();
+                    std::cout << "Hard   Mode" ;
+
+                    SetCursorPosition(27, 28);
+                    SetColor(3);
+                    std::cout << "Purgatory!!" ;
+
+                    --key;
+                    break;
+                }
+            }
+            break;
+
+        case 80://DOWN下方向键
+            if (key < 4)
+            {
+                switch (key)
+                {
+                case 1:
+                    SetCursorPosition(27, 24);
+                    SetBackColor();
+                    std::cout << "Normal Mode" ;
+                    SetCursorPosition(27, 22);
+                    SetColor(3);
+                    std::cout << "Easy   Mode" ;
+
+                    ++key;
+                    break;
+                case 2:
+                    SetCursorPosition(27, 26);
+                    SetBackColor();
+                    std::cout << "Hard   Mode" ;
+                    SetCursorPosition(27, 24);
+                    SetColor(3);
+                    std::cout << "Normal Mode" ;
+
+                    ++key;
+                    break;
+                case 3:
+                    SetCursorPosition(27, 28);
+                    SetBackColor();
+                    std::cout << "Purgatory!!" ;
+                    SetCursorPosition(27, 26);
+                    SetColor(3);
+                    std::cout << "Hard   Mode" ;
+
+                    ++key;
+                    break;
+                }
+            }
+            break;
+
+        case 13://Enter回车键
+            flag = true;
+            break;
+        default://无效按键
+            break;
+        }
+        if (flag) break;//输入Enter回车键确认，退出检查输入循环
+
+        SetCursorPosition(0, 31);//将光标置于左下角，避免关标闪烁影响游戏体验
+    }
+    switch (key)
+    {
+    case 1:
+
+        break;
+    case 2:
+        cout<<"case2"<<endl;
+        break;
+    case 3:
+        cout<<"case3"<<endl;
+        break;
+    case 4:
+        cout<<"case4"<<endl;
+    default:
+        break;
+    }
+}
+
+int GameController::PlayGame()//游戏二级循环
+{
+    HideCursor();
+    Player BlackPlayer(BLACK);//创建Player类，并使用默认构造方法，生成黑子选手
+    Player WhitePlayer(WHITE);
+    while(true)
+    {
+
+    }
+}
+
+void GameController::Game()//游戏一级循环
+{
+    Start();//开始界面
+    while (true)//游戏可视为一个死循环，直到退出游戏时循环结束
+    {
+        SelectPlayer();//选择界面
+        PlayGame();
+    }
+}
+
