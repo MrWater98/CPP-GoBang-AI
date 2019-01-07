6116b85ca6b9c533c86d228967c57f5791569c18
Narcissus2 <DMT1709533@xmu.edu.my>
2019-01-07 22:30:10 +0800

    Update Player.h

--- Player.h
+++ Player.h
@@ -2,14 +2,13 @@
 #define PLAYER_H
 #include "ChessBoard.h"
 #include<map>
-class Player
+class Player// represent the normal player
 {
     public:
         Player();
         virtual ~Player();
         Player(ChessType Color);
         ChessType chessColor;
-        //float Score[15][15];
         void Start();
         virtual void playChess();
     protected:
