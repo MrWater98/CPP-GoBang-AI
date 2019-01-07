4ace6fd8ae8fb27bd231adfd161b2129ef2e0f36
Narcissus2 <DMT1709533@xmu.edu.my>
2019-01-07 22:29:33 +0800

    Update AIPlayer1.h

--- AIPlayer1.h
+++ AIPlayer1.h
@@ -9,12 +9,11 @@ class AIPlayer1 : public Player
 public:
     AIPlayer1();
     AIPlayer1(ChessType Color);
-    float getTotalScore(pair<short,short> p);
-    virtual float getLineScore(pair<short,short> p,pair<short,short> offset,ChessType myChessColor);
+    float getTotalScore(pair<short,short> p);//Get total Score
+    virtual float getLineScore(pair<short,short> p,pair<short,short> offset,ChessType myChessColor);//Get the line score
     map<ChessType,char> stateMap;
-    map<string,float> toScore;
-    //ChessType temp;
-    float Score[15][15];
+    map<string,float> toScore;// heuristic function
+    float Score[15][15];//The chessboard score matirx
     virtual void playChess();
     virtual ~AIPlayer1();
 protected:
